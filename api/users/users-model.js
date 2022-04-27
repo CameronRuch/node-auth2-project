d55c72e1d819549ff6a72f00e69353089b7429b6
CameronRuch <cameron.ruch@yahoo.com>
2022-04-27 14:33:57 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,7 +4,6 @@ function find() {
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
-
     [
       {
         "user_id": 1,
@@ -18,6 +17,9 @@ function find() {
       }
     ]
    */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -34,6 +36,11 @@ function findBy(filter) {
       }
     ]
    */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where(filter)
+
 }
 
 function findById(user_id) {
@@ -47,6 +54,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
